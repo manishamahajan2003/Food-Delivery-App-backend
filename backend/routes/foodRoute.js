fbaa8ac704107680aeafc7e00ff2191437b7fa88
manumahajan2003@gmail.com <manishamahajan2003>
2024-08-19 09:54:29 -0700

    multer changes

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,29 +1,23 @@
 import express from "express";
-import { addFood, listFood, removeFood} from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
 
 // Image Storage Engine
 const storage = multer.diskStorage({
-    destination: "uploads", // Ensure this directory exists
+    destination: (req, file, cb) => {
+        cb(null, '/tmp'); // Use the writable /tmp directory
+    },
     filename: (req, file, cb) => {
-       return cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
 
 const upload = multer({ storage: storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
-// foodRouter.get("/list",listFood)
-foodRouter.get("/list",listFood)
-foodRouter.post("/remove",removeFood);
-
-
-
-
-
-
-
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
